0433ee93ad53f44683d58794a72d1f9d6a7b2fe3
aumkarringe <aumkarringe@gmail.com>
2025-10-08 15:54:51 -0700

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,11 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* Add basename for GitHub Pages deployment */}
+      <BrowserRouter basename="/resumeaimatch/">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* Catch-all route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
